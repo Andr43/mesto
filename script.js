4333364e4c084a83a22f66f2dedc32a1d2b055ea
Andrey Musinov <andrew.musinov@gmail.com>
2022-09-14 02:23:14 +0300

    Проект готов к 3 тестам

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let popup = document.querySelector('.popup')
-let editButton = document.querySelector('.profile__edit_button');
+let popupPage = document.querySelector('.page__darked')
+let editButton = document.querySelector('.button_edit');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-let popupContainer = document.querySelector('.popup__container');
+let popupContainer = document.querySelector('.popup');
 let popupForm = document.querySelector('.popup__form');
 let popupFieldName = document.querySelector('#name');
 let popupFieldJob = document.querySelector('#job');
@@ -10,13 +10,13 @@ let saveButton = document.querySelector('.popup__button');
 let closeButton = document.querySelector('.close-button');
 
 function ready(){
-  popup.classList.remove('popup');
+  popupPage.classList.remove('page__darked');
 };
 
 document.addEventListener("DOMContentLoaded", ready);
 
 editButton.addEventListener('click', function(){
-  popup.classList.add('popup');
+  popupPage.classList.add('page__darked');
   popupContainer.classList.add('popup_opened');
   closeButton.style = 'display: block';
   popupFieldName.setAttribute('value', 'Жак-Ив Кусто');
@@ -41,13 +41,13 @@ popupFieldJob.addEventListener('click', function(){
 closeButton.addEventListener('click', function (){
   popupContainer.classList.remove('popup_opened');
   closeButton.style = 'display: none';
-  popup.classList.remove('popup');
+  popupPage.classList.remove('page__darked');
 });
 
 saveButton.addEventListener('click', function (){
   popupContainer.classList.remove('popup_opened');
   closeButton.style = 'display: none';
-  popup.classList.remove('popup');
+  popupPage.classList.remove('page__darked');
 });
 
 function formSubmitHandler (evt) {
