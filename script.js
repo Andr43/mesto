a5c787a38fe591c78ebb91570164010451468633
Andrey Musinov <andrew.musinov@gmail.com>
2022-09-21 00:01:48 +0300

    Код исправлен после первого ревью

--- script.js
+++ script.js
@@ -1,58 +1,39 @@
-let popupPage = document.querySelector('.page__darked')
-let editButton = document.querySelector('.profile__button_edit');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-let popupContainer = document.querySelector('.popup');
-let popupForm = document.querySelector('.popup__form');
-let popupFieldName = document.querySelector('#name');
-let popupFieldJob = document.querySelector('#job');
-let saveButton = document.querySelector('.popup__button');
-let closeButton = document.querySelector('.close-button');
+let editButton = document.querySelector(".profile__button_edit");
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__job");
+let popupContainer = document.querySelector(".popup-bg");
+let popupForm = document.querySelector(".popup__form");
+let popupFieldName = document.querySelector("#name");
+let popupFieldJob = document.querySelector("#job");
+let saveButton = document.querySelector(".popup__button_save");
+let closeButton = document.querySelector(".popup__button_close");
 
-function ready(){
-  popupPage.classList.remove('page__darked');
-};
-
-document.addEventListener("DOMContentLoaded", ready);
-
-editButton.addEventListener('click', function(){
-  popupPage.classList.add('page__darked');
-  popupContainer.classList.add('popup_opened');
-  closeButton.style = 'display: block';
-  popupFieldName.setAttribute('value', 'Жак-Ив Кусто');
-  popupFieldJob.setAttribute('value', 'Исследователь океана');
+function openPopup() {
+  editButton.addEventListener("click", function () {
+    popupContainer.classList.add("popup_opened");
+    closeButton.classList.add("popup_opened");
+  });
 }
-);
+openPopup();
 
-popupFieldName.addEventListener('click', function(){
-  popupFieldName.removeAttribute('value', 'Жак-Ив Кусто');
-  popupFieldName.addEventListener('keyup', function(e) {
-    profileName.textContent = e.target.value
-});
-});
-
-popupFieldJob.addEventListener('click', function(){
-  popupFieldJob.removeAttribute('value', 'Исследователь океана');
-  popupFieldJob.addEventListener('keyup', function(e) {
-    profileJob.textContent = e.target.value
-});
+popupForm.addEventListener("click", () => {
+  function formSubmitHandler(evt) {
+    profileName.textContent = popupFieldName.value;
+    profileJob.textContent = popupFieldJob.value;
+    evt.preventDefault();
+  }
+  popupForm.addEventListener("submit", formSubmitHandler);
 });
 
-closeButton.addEventListener('click', function (){
-  popupContainer.classList.remove('popup_opened');
-  closeButton.style = 'display: none';
-  popupPage.classList.remove('page__darked');
+function closePopup() {
+  popupContainer.classList.remove("popup_opened");
+  closeButton.classList.remove("popup_opened");
+}
+
+closeButton.addEventListener("click", function () {
+  closePopup();
 });
 
-saveButton.addEventListener('click', function (){
-  popupContainer.classList.remove('popup_opened');
-  closeButton.style = 'display: none';
-  popupPage.classList.remove('page__darked');
+saveButton.addEventListener("click", function () {
+  closePopup();
 });
-
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-  popupFieldName.value = profileName.textContent;
-  popupFieldJob.value = profileJob.textContent;
-};
-popupForm.addEventListener('submit', formSubmitHandler); 
