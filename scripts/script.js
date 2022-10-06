5f9cd5096474fd105a25e2f5516edc603cec5599
Andrey Musinov <andrew.musinov@gmail.com>
2022-10-06 14:31:28 +0100

    Проект готов к 2 ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
-const editButton = document.querySelector(".profile__button_edit");
-const addButton = document.querySelector(".profile__button_add");
+const btnOpenPopupEditProfile = document.querySelector(".profile__button_edit");
+const btnOpenPopupAddCard = document.querySelector(".profile__button_add");
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
 const popupContainer = document.querySelector(".popup");
-const popupEdit = document.querySelector(".popup-edit");
-const popupAdd = document.querySelector(".popup-add");
+const popupEditProfile = document.querySelector(".popup-edit");
+const popupAddCard = document.querySelector(".popup-add");
 const popupImage = document.querySelector(".popup-image");
-const popupFormEdit = document.querySelector(".popup__form_edit");
-const popupFormAdd = document.querySelector(".popup__form_add");
+const formEditProfile = document.querySelector(".popup__form_edit");
+const formAddCard = document.querySelector(".popup__form_add");
 const popupFieldName = document.querySelector(".popup__field_name");
 const popupFieldJob = document.querySelector(".popup__field_job");
 const popupFieldHeading = document.querySelector(".popup__field_heading");
@@ -15,22 +15,24 @@ const popupFieldSource = document.querySelector(".popup__field_source");
 const saveButton = document.querySelector(".popup__button_save");
 const closeButtonEdit = document.querySelector(".popup__button_close_edit");
 const closeButtonAdd = document.querySelector(".popup__button_close_add");
+const popupImageBig = document.querySelector(".popup-image__image");
+const popupHeadingBig = document.querySelector(".popup-image__heading");
 
 // код, открывающий и закрывающий окно изменения профиля, а также меняющий данные профиля
 function openPopup(popup) {
   popup.classList.add("popup_opened");
 }
 
-editButton.addEventListener("click", function () {
-  openPopup(popupEdit);
-  popupFieldName.value = profileName.textContent;
-  popupFieldJob.value = profileJob.textContent;
+btnOpenPopupEditProfile.addEventListener("click", function () {
+  openPopup(popupEditProfile);
+  popupFieldName.value = '';
+  popupFieldJob.value = '';
 });
 
-popupFormEdit.addEventListener("submit", (evt) => {
+formEditProfile.addEventListener("submit", (evt) => {
   profileName.textContent = popupFieldName.value;
   profileJob.textContent = popupFieldJob.value;
-  closePopup(popupEdit);
+  closePopup(popupEditProfile);
   evt.preventDefault();
 });
 
@@ -39,50 +41,26 @@ function closePopup(popup) {
 }
 
 closeButtonEdit.addEventListener("click", function () {
-  closePopup(popupEdit);
+  closePopup(popupEditProfile);
 });
 
-addButton.addEventListener("click", function () {
-  openPopup(popupAdd);
+btnOpenPopupAddCard.addEventListener("click", function () {
+  openPopup(popupAddCard);
+  popupFieldHeading.value = '';
+  popupFieldSource.value = '';
 });
 
 closeButtonAdd.addEventListener("click", function () {
-  closePopup(popupAdd);
+  closePopup(popupAddCard);
 });
 // _______________________________________________________________________
 
 // код, открывающий и закрывающий окно добавления карточек, а также удаляющий карточки, добавляющий карточки в избранное
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
 
 const elementsList = document.querySelector(".elements__list");
 const elementsTemplate = document.querySelector(".elements__template").content;
 
-function renderElements(text, link) {
+function createCard(text, link) {
   const newElement = elementsTemplate
     .querySelector(".elements__element")
     .cloneNode(true);
@@ -90,7 +68,7 @@ function renderElements(text, link) {
   const imageElement = newElement.querySelector(".elements__image");
   headingElement.textContent = text;
   imageElement.src = link;
-  imageElement.addEventListener("click", () => imagePopupOpen(text, link));
+  imageElement.addEventListener("click", () => openImagePopup(text, link));
   setListenersForInitialCards(newElement);
   return newElement;
 }
@@ -113,30 +91,30 @@ function likeButtonToggle(button) {
   button.classList.toggle("elements__button_like_active");
 }
 
-function imagePopupOpen(name, link) {
+function openImagePopup(name, link) {
   openPopup(popupImage);
-  const popupImageBig = document.querySelector(".popup-image__image");
-  const popupHeadingBig = document.querySelector(".popup-image__heading");
   popupImageBig.src = link;
+  popupImageBig.alt = name;
   popupHeadingBig.textContent = name;
 }
 
-function renderCards(card) {
+function submitAddCardForm(card) {
   card.forEach((element) => {
-    elementsList.append(renderElements(element.name, element.link));
+    elementsList.append(createCard(element.name, element.link));
   });
 }
 
-renderCards(initialCards);
+submitAddCardForm(initialCards);
 
 function createNewCard(evt) {
   elementsList.prepend(
-    renderElements(popupFieldHeading.value, popupFieldSource.value)
+    createCard(popupFieldHeading.value, popupFieldSource.value)
   );
   evt.preventDefault();
-  closePopup(popupAdd);
+  closePopup(popupAddCard);
 }
 
-popupFormAdd.addEventListener("submit", createNewCard);
+formAddCard.addEventListener("submit", createNewCard);
+
 
 //___________________________________________________________________________
